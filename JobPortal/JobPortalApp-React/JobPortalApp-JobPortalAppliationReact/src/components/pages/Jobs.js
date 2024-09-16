d9749f73b6c474fda0bff0e95a21243f83552ae7
ajayc <ajayc@AJAY-LAPTOP>
2024-09-16 23:14:32 +0530

    commit

--- JobPortal/JobPortalApp-React/JobPortalApp-JobPortalAppliationReact/src/components/pages/Jobs.js
+++ JobPortal/JobPortalApp-React/JobPortalApp-JobPortalAppliationReact/src/components/pages/Jobs.js
@@ -1,7 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchJobs, applyForJobs } from "../../redux/actions/jobAction";
-import { Button, Container, Row, Col } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Container,
+  FormControl,
+  Row,
+  Col,
+  InputGroup,
+} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const Jobs = () => {
@@ -9,7 +17,7 @@ const Jobs = () => {
   const navigate = useNavigate();
   const { jobs, status, error } = useSelector((state) => state.jobs);
   const { isAuthenticated, userType } = useSelector((state) => state.login);
-
+  const [search, setSearch] = useState("");
   useEffect(() => {
     dispatch(fetchJobs());
   }, [dispatch]);
@@ -28,41 +36,55 @@ const Jobs = () => {
   return (
     <Container>
       <h1>Job Listings</h1>
+      <Form>
+        <InputGroup className="my-3">
+          <FormControl
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search Jobs"
+          />
+        </InputGroup>
+      </Form>
       <Row>
-        {jobs.map((job) => (
-          <Col key={job.id} sm={12} md={6} lg={4} className="mb-3">
-            <div className="job-card p-3 border rounded">
-              <h2>{job.title}</h2>
-              <p>{job.description}</p>
-              <p>
-                <strong>Posted By:</strong> {job.postedBy.name}
-              </p>
-              <p>
-                <strong>Company:</strong> {job.companyName}
-              </p>
-              <p>
-                <strong>Posted On:</strong>{" "}
-                {new Date(job.postedOn).toLocaleDateString()}
-              </p>
-              <p>
-                <strong>Total Applications:</strong> {job.totalApplications}
-              </p>
-              {isAuthenticated && userType === "Applicant" && (
-                <Button variant="primary" onClick={() => handleApply(job.id)}>
-                  Apply
-                </Button>
-              )}
-              {isAuthenticated && userType === "Admin" && (
-                <Button
-                  variant="secondary"
-                  onClick={() => handleViewDetails(job.id)}
-                >
-                  View Details
-                </Button>
-              )}
-            </div>
-          </Col>
-        ))}
+        {jobs
+          .filter((job) => {
+            return search.toLowerCase() === ""
+              ? job
+              : job.title.toLowerCase().includes(search);
+          })
+          .map((job) => (
+            <Col key={job.id} sm={12} md={6} lg={4} className="mb-3">
+              <div className="job-card p-3 border rounded">
+                <h2>{job.title}</h2>
+                <p>{job.description}</p>
+                <p>
+                  <strong>Posted By:</strong> {job.postedBy.name}
+                </p>
+                <p>
+                  <strong>Company:</strong> {job.companyName}
+                </p>
+                <p>
+                  <strong>Posted On:</strong>{" "}
+                  {new Date(job.postedOn).toLocaleDateString()}
+                </p>
+                <p>
+                  <strong>Total Applications:</strong> {job.totalApplications}
+                </p>
+                {isAuthenticated && userType === "Applicant" && (
+                  <Button variant="primary" onClick={() => handleApply(job.id)}>
+                    Apply
+                  </Button>
+                )}
+                {isAuthenticated && userType === "Admin" && (
+                  <Button
+                    variant="secondary"
+                    onClick={() => handleViewDetails(job.id)}
+                  >
+                    View Details
+                  </Button>
+                )}
+              </div>
+            </Col>
+          ))}
       </Row>
     </Container>
   );
